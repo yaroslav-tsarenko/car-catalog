ae6f917d1af0ad7f5b66564a030c5de3223add5c
yaroslav <yaroslav7v@gmail.com>
2023-07-05 23:34:43 +0300

    added component carItem to Home.jsx

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,33 +1,18 @@
 import styles from './Home.module.css'
-import {cars} from './cars.data.js'
+import CarItem from "./car-item/CarItem.jsx";
+/*import {cars} from './cars.data.js'*/
+const cars = []
 
 const Home = () => {
     return (
         <div>
             <h1>Cars catalog</h1>
             <div>
-                {cars.length ? cars.map(car => (
-                    <div key={car.id} className={styles.item}>
-                        <div
-                            className={styles.image}
-                            style={{
-                                backgroundImage: `url(${car.image})`,
-                            }}
-                        />
-                        <div className={styles.info}>
-                            <h2>{car.name}</h2>
-                            <p>
-                                {new Intl.NumberFormat('en-US', {
-                                    style: 'currency',
-                                    currency: 'USD',
-                                }).format(car.price)}
-                            </p>
-                            <button>Read more</button>
-                        </div>
-                    </div>
-                ))
-                : <p>There no cars</p>
-                }
+                {cars.length ? (
+                    cars.map(car => <CarItem key={car.id} car={car}/>)
+                ) : (
+                    <p>There no cars</p>
+                )}
             </div>
         </div>
     );
