8fd5d6a6c11e9f8c0612b40307f6fe1da32dc0e4
yaroslav <yaroslav7v@gmail.com>
2023-07-09 23:59:08 +0300

    changes for main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/styles/global.css'
 import Home from "./components/screens/home/Home.jsx";
-import Router from "./components/ui/Router.jsx";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Router/>
+        <Home/>
     </React.StrictMode>,
 )
